d86c7d4bbecb76c17a525c34cfbad5b29de030dc
tomoponz <yuto181130+tomoponz@gmail.com>
2026-03-30 18:00:32 +0900

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,7 @@
-const CACHE_NAME = "day-manager-cache-v21";
+const CACHE_NAME = "day-manager-cache-v25";
 const CORE_ASSETS = [
   "./",
   "./index.html",
-  "./manual-simple.html",
-  "./manual-detailed.html",
-  "./privacy.html",
-  "./terms.html",
   "./style.css",
   "./app.js",
   "./manifest.webmanifest",
@@ -114,10 +110,3 @@ async function staleWhileRevalidate(request) {
 
   return cached || fetchPromise || Response.error();
 }
-
-
-self.addEventListener("message", (event) => {
-  if (event.data?.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
